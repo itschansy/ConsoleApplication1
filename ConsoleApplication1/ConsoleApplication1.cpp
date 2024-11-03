cf0310bc43b07b2a88f99baf693420639b0c32fb
canke <canke@CAN>
2024-11-03 23:16:07 +0300

    Proje dosyası ekle.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,25 @@
+﻿// std::vector için binarySearchFirst fonksiyonu
+template<typename T>
+int binarySearchFirst(std::vector<T> vec, T target) {
+    int index = binarySearch(vec, target);
+    if (index == -1) return -1; // Hedef bulunamadıysa -1 döndür.
+
+    // İlk eşleşmeyi bulmak için geriye gidin.
+    while (index > 0 && vec[index - 1] == target) {
+        --index;
+    }
+    return index;
+}
+
+// std::array için binarySearchFirst fonksiyonu
+template<typename T, std::size_t N>
+int binarySearchFirst(std::array<T, N> arr, T target) {
+    int index = binarySearch(arr, target);
+    if (index == -1) return -1; // Hedef bulunamadıysa -1 döndür.
+
+    // İlk eşleşmeyi bulmak için geriye gidin.
+    while (index > 0 && arr[index - 1] == target) {
+        --index;
+    }
+    return index;
+}
